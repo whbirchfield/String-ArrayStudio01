6ceef873132dcd99ebfb2856b5e9fc6781b15183
Will B <willhbjr@gmail.com>
2021-02-18 07:49:34 +0000

    updated syntax for error message.

--- index.js
+++ index.js
@@ -16,20 +16,28 @@ console.log("\n");
 
 //b) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 let strMoveNumber = input.question("Number of characters to move to the end: ");
-strMoveNumber = Number(strMoveNumber);
 
-if (strMoveNumber < str.lenght){
-  suffix = (str.slice(0,strMoveNumber));
+strMoveNumber = Number(strMoveNumber);
+suffix = (str.slice(0,strMoveNumber));
 prefix = (str.slice(strMoveNumber,strgLength));
 newStr = prefix + suffix;
 console.log(`${str} becomes ${newStr} after the system updates!`);
-} else{
-  suffix = (str.slice(0,3));
-prefix = (str.slice(3,strgLength));
-newStr = prefix + suffix;
-console.log(`${str} becomes ${newStr} after the system updates!`);
-}
+console.log("\n");
 
 
 //c) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
 
+strMoveNumber = input.question("Number of characters to move to the end: ");
+strMoveNumber = Number(strMoveNumber);
+
+if (strMoveNumber < str.lenght){
+  suffix = (str.slice(0,strMoveNumber));
+  prefix = (str.slice(strMoveNumber,strgLength));
+  newStr = prefix + suffix;
+  console.log(`${str} becomes ${newStr} after the system updates!`);
+} else{
+  suffix = (str.slice(0,3));
+  prefix = (str.slice(3,strgLength));
+  newStr = prefix + suffix;
+  console.log(`Your number was too high! System defaults to 3 characters. ${str} becomes ${newStr}.`);
+}
